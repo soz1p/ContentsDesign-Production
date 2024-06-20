5693acf7f32462efdb16d4137d4ba8c7a8f2158b
soz1p <jeonsojin1363@hansung.ac.kr>
2024-06-20 15:19:25 +0900

    fix : single Scene camera scripts

--- Assets/Camera/cshDungeonCamera.cs
+++ Assets/Camera/cshDungeonCamera.cs
@@ -1,31 +1,22 @@
 using UnityEngine;
-using Photon.Pun;
+using System.Collections;
 
-public class cshDungeonCamera : MonoBehaviourPunCallbacks
+public class cshDungeonCamera : MonoBehaviour
 {
-    public GameObject target; // 타겟을 수동으로 설정할 필요 없음
-    public float damping = 1;
-    private Vector3 offset;
+	public GameObject target;
+	public float damping = 1;
+	Vector3 offset;
+	void Start()
+	{
+		offset = transform.position - target.transform.position;
+	}
 
-    void Start()
-    {
-        // PhotonView를 통해 로컬 플레이어의 카메라만 타겟 설정
-        if (photonView.IsMine)
-        {
-            target = GameObject.FindWithTag("Player"); // 로컬 플레이어를 타겟으로 설정
-            offset = transform.position - target.transform.position;
-        }
-    }
+	void LateUpdate()
+	{
+		Vector3 desiredPosition = target.transform.position + offset;
+		Vector3 position = Vector3.Lerp(transform.position, desiredPosition, Time.deltaTime * damping);
+		transform.position = position;
 
-    void LateUpdate()
-    {
-        if (photonView.IsMine)
-        {
-            Vector3 desiredPosition = target.transform.position + offset;
-            Vector3 position = Vector3.Lerp(transform.position, desiredPosition, Time.deltaTime * damping);
-            transform.position = position;
-
-            transform.LookAt(target.transform.position);
-        }
-    }
+		transform.LookAt(target.transform.position);
+	}
 }
