856e8cb0d5026363c14e7ed3168362283bb904c3
soz1p <jeonsojin1363@hansung.ac.kr>
2024-06-21 12:55:37 +0900

    feat : survived Mountain Scene

--- Assets/scripts/cshCompassSpawner.cs
+++ Assets/scripts/cshCompassSpawner.cs
@@ -4,7 +4,7 @@ public class cshCompassSpawner : MonoBehaviour
 {
     public GameObject compassPrefab;
     public Vector3 initialPosition = new Vector3(14f, 5f, 15f);
-    public float randomRange = 5f;
+    public float randomRange = 3f;
 
     void Start()
     {
